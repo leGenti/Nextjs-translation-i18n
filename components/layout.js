a2fb3b36d6c9832308dc1e43727b60c672a51feb
Gentian Collaku <gentian.collaku@hotmail.com>
2021-07-15 12:23:33 +0200

    Nextjs internationalization i18n

---
+++ components/layout.js
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Link from "next/link";
+import classes from "../styles/layout.module.css";
+import { useRouter } from 'next/router'
+
+export default function Layout({ children }) {
+  let router = useRouter()
+  return (
+    <div>
+      <Head>
+        <title>i18n voorbeeld</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className={classes.header}>
+        <nav className={classes.nav}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </nav>
+      </header>
+
+      <main className={classes.content}>{children}</main>
+
+      <footer className={classes.footer}>
+        <p>© 2021 </p>
+        <ul>
+          {router.locales.map(locale => (
+            <li key={locale}>
+              <Link href={router.asPath} locale={locale}><a>{locale}</a></Link>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    </div>
+  );
+}
